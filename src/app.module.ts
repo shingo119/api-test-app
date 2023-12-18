8fb02d59c22c7617b194845ab451a611e3332bea
yano <yano@example.com>
2023-12-18 22:04:56 +0900

    アプリケーション全体で.envを使えるようにルートにCongigModuleを追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ResasModule } from './resas/resas.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ResasModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // アプリケーション全体でConfigModuleを利用可能にする
+    }),
+    ResasModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
