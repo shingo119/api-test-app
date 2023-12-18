99f58e7493b9795b27f245eda71757bc641f1299
yano <yano@example.com>
2023-12-18 21:54:44 +0900

    repositoryインターフェースの実装

--- src/resas/repository/resas.repository.ts
+++ src/resas/repository/resas.repository.ts
@@ -1,5 +1,6 @@
 import { EstateQueryDto } from '../dto/estate-query.dto';
 
 export interface ResasRepository {
+  // クエリパラメータを受け取り、RESAS-APIとの通信を実装する
   findAll(query: EstateQueryDto): Promise<string>;
 }
